a8ec3d8568398a461ed3ec1de1e38d967e4a33a3
cbollin <cassidybollin32@gmail.com>
2020-06-28 23:14:06 -0500

    updating stat

---
+++ commands/balance.js
@@ -0,0 +1,9 @@
+// module.exports = {
+// 	name: 'balance',
+// 	description: 'balance',
+// 	execute(message, args) {
+// 		const target = message.mentions.User.first() || message.author;
+// 		message.channel.send(`${target.tag} has ${currency.getBalance(target.id)} 💰`);
+// 	},
+// };
+
